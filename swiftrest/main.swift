b111668d7689c440d9cd1112b56466743e0b7aa6
Steffen Neubauer <stefreak@googlemail.com>
2015-06-16 01:47:43 +0200

    Initial Commit

---
+++ swiftrest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftrest
+//
+//  Created by Steffen Neubauer on 16/06/15.
+//  Copyright © 2015 Steffen Neubauer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
